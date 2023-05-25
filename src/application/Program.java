cd9a6be5622ad2589f248f27ac1839bd23be7eba
Vinicius Almeida <vinicius.va2014@gmail.com>
2023-05-24 23:47:14 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +12,8 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		Seller seller = new Seller(21, "Carlos jose", "CCj@gmail.com", LocalDate.of(1985, 05, 22) , 5000.0, obj);
+		System.out.println(seller);
 
 	}
 
